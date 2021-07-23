dc01f9aad85882df18713923ef381942106a963b
Skreeauk <waldojongsoo@yahoo.com>
2021-07-23 17:04:37 +0800

    Added Party Functionality

--- app/app.py
+++ app/app.py
@@ -55,7 +55,39 @@ def home():
     r = requests.post(url, json={'query': Query_Pokemon_List})
     r_data = r.json()
     data = r_data['data']['pokemons']['results']
-    return render_template('home.html', data=data, party=party)
+
+    party_data = []
+
+    if len(party) > 0:
+        for pokemon in party:
+
+            query1 = 'pokemon(name: "{}")'.format(pokemon[1])
+            query2 = '''
+                {
+                    id
+                    name
+                    sprites {
+                        front_default
+                    }
+                    moves {
+                        move {
+                            name
+                        }
+                    }
+                    types {
+                        type {
+                            name
+                        }
+                    }
+                }
+            }'''
+            query = '{' + query1 + query2
+            r = requests.post(url, json={'query': query})
+            r_data = r.json()
+            image = r_data['data']['pokemon']['sprites']['front_default']
+            party_data.append((pokemon[0], image))
+
+    return render_template('home.html', data=data, party=party_data)
 
 @app.route("/<string:pokemon_name>")
 def pokemon(pokemon_name):
@@ -90,7 +122,8 @@ def pokemon(pokemon_name):
 @app.route("/rename", methods=['POST'])
 def rename():
     pokenickname = request.form['pokeNickname']
-    party.append(pokenickname)
+    pokename = request.form['pokeName']
+    party.append((pokenickname, pokename))
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
